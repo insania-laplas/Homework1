cf345ae6a693cf1768902f943b59581a2c7132f9
repo Sky Pro <lyubushkin.serezha@list.ru>
2023-10-09 23:26:26 +0300

    сделал js опять же не всё так гладко(

---
+++ script.js
@@ -0,0 +1,43 @@
+//Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+//Задание 2
+let release = 2007;
+alert(release)
+//Задание 3
+let creatorJava = 'Брендан Эйх';
+alert(creatorJava);
+//Задание 4
+let s = 10;
+let d = 2;
+alert(s + d);
+alert(s - d);
+alert(s * d);
+alert(s / d);
+//Задание 5
+let result = 2 ** 5;
+alert(result);
+//Задание 6
+let x = 9;
+let y = 2;
+let modul = x % y;
+alert(modul);
+//Задание 7 не понял задания (*Э*)
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+//Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+//Задание 9 (c девятым заданием тоже немного не понимаю как вкладывать в объект динамические переменные, а так же как удалять переменные по заданию которое идёт дальше (сомневаюсь что можно просто стереть или я намудрил?))
+const user = {
+    name: 'jeck'
+    age: 27:
+}
