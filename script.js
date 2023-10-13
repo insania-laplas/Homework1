e72b894f7387785c06bdf04d4acf844e8ef99457
repo Sky Pro <lyubushkin.serezha@list.ru>
2023-10-13 23:29:52 +0300

    заготовка

---
+++ script.js
@@ -0,0 +1,11 @@
+//Задание 1
+const password = String('1104');
+let youPassword = String(prompt('Введите пароль'));
+password = youPassword ? alert('Пароль введен верно') : alert('Пароль введен неправильно');
+//Задание 2
+const c = 12;
+if (c > 0 && c < 10) {console.log('Верно');
+    
+} else {console.log('Неверно');
+    
+};
